d3061c8c9fbd8d681d65bacebeb02f856a4e9520
ihgoo <ihgoo2008@gmail.com>
2015-03-26 10:16:39 +0800

    config imageloader & use Inject ViewHodler

--- Rosi/app/src/main/java/com/ihgoo/rosi/adapter/WaterfallAdapter.java
+++ Rosi/app/src/main/java/com/ihgoo/rosi/adapter/WaterfallAdapter.java
@@ -3,9 +3,7 @@ package com.ihgoo.rosi.adapter;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,7 +20,6 @@ import com.ihgoo.rosi.R;
 import com.ihgoo.rosi.bean.ImageBean;
 import com.ihgoo.rosi.bean.ImageListBean;
 import com.ihgoo.rosi.bean.SettingHelper;
-import com.ihgoo.rosi.net.ImageLoaderHelper;
 import com.ihgoo.rosi.ui.ContentFragment;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.FailReason;
@@ -30,178 +27,164 @@ import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 import java.util.List;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 
 public class WaterfallAdapter extends ArrayAdapter<ImageBean> {
-	private OnBottonListener mOnBottonListener;
-	private List<ImageBean> list;
-	private Context mContext;
-	private Drawable drawable;
-	private int mScreenWidth;
-	private SparseArray<ImageListBean> mCache = new SparseArray();
-	private boolean cacheFound;
-	protected ImageLoader imageLoader = ImageLoader.getInstance(); 
-	
-	/**
-	 * 监听是否到了底部
-	 * @author KelvinHu
-	 *
-	 */
-	public interface OnBottonListener {  
-		void loadMore();  
-	}  
-	
-	// 接口实例化
-	public void setOnBottonListener(OnBottonListener onBottonListener){
-		mOnBottonListener = onBottonListener;
-	}
-	
-	public WaterfallAdapter(Context context, int resource, List<ImageBean> list) {
-		super(context, resource, list);
-		this.list = list;
-		this.mContext = context;
-		mScreenWidth=((MainActivity)mContext).screenWidth;
-		drawable = context.getResources().getDrawable(R.drawable.load_default);
-	}
+    private OnBottonListener mOnBottonListener;
+    private List<ImageBean> list;
+    private Context mContext;
+    private int mScreenWidth;
+    private SparseArray<ImageListBean> mCache = new SparseArray();
+    private boolean cacheFound;
+
+    /**
+     * 监听是否到了底部
+     */
+    public interface OnBottonListener {
+        void loadMore();
+    }
+
+    public void setOnBottonListener(OnBottonListener onBottonListener) {
+        mOnBottonListener = onBottonListener;
+    }
+
+    public WaterfallAdapter(Context context, int resource, List<ImageBean> list) {
+        super(context, resource, list);
+        this.list = list;
+        this.mContext = context;
+        mScreenWidth = ((MainActivity) mContext).screenWidth;
+    }
 
 
-	
-	
+    @Override
+    public int getCount() {
+        return list != null ? list.size() : 0;
+    }
 
 
-	@Override
-	public int getCount() {
-		return list != null ? list.size() : 0;
-	}
+    @Override
+    public long getItemId(int arg0) {
+        return 0;
+    }
 
 
-	@Override
-	public long getItemId(int arg0) {
-		return 0;
-	}
+    @Override
+    public View getView(final int position, View view, ViewGroup group) {
 
-	
-	
-	@Override
-	public View getView(final int position, View view, ViewGroup group) {
-		final Holder holder;
-		
-		if (mOnBottonListener != null) {
-			int addPosition =0;
-			if (SettingHelper.getInstance().isPrfetch()) {
-				addPosition = position+9;
-			}else {
-				addPosition = position+2;
-			}
-			
-			if (list!=null && list.size() < addPosition) {
-				Log.i("WaterfallAdapter", "position"+position);
-				mOnBottonListener.loadMore();
-			}
-		}
-		
-		final ImageBean imageBean = list.get(position);
-		
-		if (view == null) {
-			holder = new Holder();
-			LayoutInflater inflater = LayoutInflater.from(mContext);
-			view = inflater.inflate(R.layout.image_item, null);
-			holder.ivIcon = (ImageView) view.findViewById(R.id.row_icon);
-			holder.pbLoad = (ProgressBar) view.findViewById(R.id.pb_load);
-			view.setTag(holder);
-		} else {
-			holder = (Holder) view.getTag();
-		}
+        if (mOnBottonListener != null) {
+            int addPosition = 0;
+            if (SettingHelper.getInstance().isPrfetch()) {
+                addPosition = position + 9;
+            } else {
+                addPosition = position + 2;
+            }
 
-		
-		holder.ivIcon.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				ContentFragment fragment = new ContentFragment();
-				Bundle bundle = new Bundle();
-				bundle.putInt("mode", ContentFragment.MODE_IMAGEDETAILLIST);
-				bundle.putString("url", imageBean.getDetailurl());
-				fragment.setArguments(bundle);
-				((MainActivity)mContext).addFragment(fragment);
-			}
-		});
-		
-		
-		if (!imageBean.getImgurl().equals(holder.url)) {
-			imageLoader.displayImage(imageBean.getImgurl(), holder.ivIcon,ImageLoaderHelper.getInstance().getDefaultOptions(), new ImageLoadingListener(){
-				
-		
-				@Override
-				public void onLoadingStarted(String imageUri, View view) {
-					holder.ivIcon.setImageDrawable(drawable);
-					holder.pbLoad.setVisibility(View.VISIBLE);
-				}
-		
-				@Override
-				public void onLoadingFailed(String imageUri, View view,
-						FailReason failReason) {
-					String message = null;
-					switch (failReason.getType()) {
-					case IO_ERROR:
-						message = "Input/Output error";
-						break;
-					case DECODING_ERROR:
-						message = "can not be decoding";
-						break;
-					case NETWORK_DENIED:
-						message = "Downloads are denied";
-						break;
-					case OUT_OF_MEMORY:
-						message = "内存不足";
-						Toast.makeText(mContext, message, Toast.LENGTH_SHORT)
-								.show();
-						break;
-					case UNKNOWN:
-						message = "Unknown error";
-						Toast.makeText(mContext, message, Toast.LENGTH_SHORT)
-								.show();
-						break;
-					}
-					holder.pbLoad.setVisibility(View.GONE);
-				}
-		
-				@Override
-				public void onLoadingComplete(String imageUri, View view,
-						Bitmap loadedImage) {
-					if (!imageUri.equals(holder.url)) {
-						holder.pbLoad.setVisibility(View.GONE);
-						holder.url = imageUri;
-					}
+            if (list != null && list.size() < addPosition) {
+                mOnBottonListener.loadMore();
+            }
+        }
 
-                    int height = loadedImage.getHeight();
-                    int width = loadedImage.getWidth();
-                    double scale =(double) (mScreenWidth/2)/width;
-                    int currentWidth = (int)(width * scale);
-                    int currentHeight =  (int)(height * scale);
-                    LayoutParams lp = (LayoutParams) view.getLayoutParams();
-                    lp.width = currentWidth;
-                    lp.height = currentHeight;
-                    view.setLayoutParams(lp);
+        final ImageBean imageBean = list.get(position);
+
+        if (view == null) {
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            view = inflater.inflate(R.layout.image_item, null);
+        }
+
+
+        final ViewHolder viewHolder = new ViewHolder(view);
+        viewHolder.rowIcon.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ContentFragment fragment = new ContentFragment();
+                Bundle bundle = new Bundle();
+                bundle.putInt("mode", ContentFragment.MODE_IMAGEDETAILLIST);
+                bundle.putString("url", imageBean.getDetailurl());
+                fragment.setArguments(bundle);
+                ((MainActivity) mContext).addFragment(fragment);
+            }
+        });
+
+
+        ImageLoader.getInstance().displayImage(imageBean.getImgurl(), viewHolder.rowIcon, new ImageLoadingListener() {
+
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                viewHolder.pbLoad.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view,
+                                        FailReason failReason) {
+                String message = null;
+                switch (failReason.getType()) {
+                    case IO_ERROR:
+                        message = "Input/Output error";
+                        break;
+                    case DECODING_ERROR:
+                        message = "can not be decoding";
+                        break;
+                    case NETWORK_DENIED:
+                        message = "Downloads are denied";
+                        break;
+                    case OUT_OF_MEMORY:
+                        message = "内存不足";
+                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT)
+                                .show();
+                        break;
+                    case UNKNOWN:
+                        message = "Unknown error";
+                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT)
+                                .show();
+                        break;
                 }
-				
-				
-				
-		
-				@Override
-				public void onLoadingCancelled(String paramString,
-						View paramView) {
-				}
-			});
-		}
-		
-		return view;
-	}
+                viewHolder.pbLoad.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view,
+                                          Bitmap loadedImage) {
+                viewHolder.pbLoad.setVisibility(View.INVISIBLE);
+                int height = loadedImage.getHeight();
+                int width = loadedImage.getWidth();
+                double scale = (double) (mScreenWidth / 2) / width;
+                int currentWidth = (int) (width * scale);
+                int currentHeight = (int) (height * scale);
+                LayoutParams lp = (LayoutParams) view.getLayoutParams();
+                lp.width = currentWidth;
+                lp.height = currentHeight;
+                view.setLayoutParams(lp);
+            }
 
 
+            @Override
+            public void onLoadingCancelled(String paramString,
+                                           View paramView) {
+                viewHolder.pbLoad.setVisibility(View.INVISIBLE);
+            }
+        });
 
+        return view;
+    }
+
+
+    /**
+     * This class contains all butterknife-injected Views & Layouts from layout file 'image_item.xml'
+     * for easy to all layout elements.
+     *
+     * @author ButterKnifeZelezny, plugin for Android Studio by Avast Developers (http://github.com/avast)
+     */
+    static class ViewHolder {
+        @InjectView(R.id.row_icon)
+        ImageView rowIcon;
+        @InjectView(R.id.pb_load)
+        ProgressBar pbLoad;
+
+        ViewHolder(View view) {
+            ButterKnife.inject(this, view);
+        }
+    }
 }
 
-class Holder {
-	public ImageView ivIcon;
-	public ProgressBar pbLoad;
-	public String url;
-}
--- Rosi/app/src/main/java/com/ihgoo/rosi/adapter/WaterfallSimpleAdapter.java
+++ Rosi/app/src/main/java/com/ihgoo/rosi/adapter/WaterfallSimpleAdapter.java
@@ -4,7 +4,6 @@ package com.ihgoo.rosi.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,7 +18,6 @@ import android.widget.Toast;
 import com.ihgoo.rosi.MainActivity;
 import com.ihgoo.rosi.R;
 import com.ihgoo.rosi.bean.ImageSimpleBean;
-import com.ihgoo.rosi.net.ImageLoaderHelper;
 import com.ihgoo.rosi.ui.PhotoDetailActivity;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.FailReason;
@@ -28,146 +26,146 @@ import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 import java.io.Serializable;
 import java.util.List;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 
 public class WaterfallSimpleAdapter extends BaseAdapter {
 
-	
-	private List<ImageSimpleBean> list;
-	private Context mContext;
-	private Drawable drawable;
-	private int mScreenWidth;
-	private String TAG;
-	private String mUrl;
-	
-	
-	public WaterfallSimpleAdapter(List<ImageSimpleBean> urls , Context context, String url) {
-		this.list = urls;
-		this.mContext = context;
-		this.mUrl = url;
-		drawable = context.getResources().getDrawable(R.drawable.load_default);
-		mScreenWidth=((MainActivity)mContext).screenWidth;
-		TAG = this.getClass().getSimpleName();
-	}
+
+    private List<ImageSimpleBean> list;
+    private Context mContext;
+    private int mScreenWidth;
+    private String TAG;
+    private String mUrl;
 
 
-	@Override
-	public int getCount() {
-		return list != null ? list.size() : 0;
-	}
+    public WaterfallSimpleAdapter(List<ImageSimpleBean> urls, Context context, String url) {
+        this.list = urls;
+        this.mContext = context;
+        this.mUrl = url;
+        mScreenWidth = ((MainActivity) mContext).screenWidth;
+        TAG = this.getClass().getSimpleName();
+    }
 
-	@Override
-	public Object getItem(int arg0) {
-		return null;
-	}
 
-	@Override
-	public long getItemId(int arg0) {
-		return 0;
-	}
+    @Override
+    public int getCount() {
+        return list != null ? list.size() : 0;
+    }
 
-	@Override
-	public View getView(final int position, View view, ViewGroup group) {
-		final Holder holder;
-		final ImageSimpleBean imageSimpleBean = list.get(position);
+    @Override
+    public Object getItem(int arg0) {
+        return null;
+    }
 
-		if (view == null) {
-			holder = new Holder();
-			LayoutInflater inflater = LayoutInflater.from(mContext);
-			view = inflater.inflate(R.layout.image_item, null);
-			holder.ivIcon = (ImageView) view.findViewById(R.id.row_icon);
-			holder.pbLoad = (ProgressBar) view.findViewById(R.id.pb_load);
+    @Override
+    public long getItemId(int arg0) {
+        return 0;
+    }
 
-			view.setTag(holder);
-		} else {
-			holder = (Holder) view.getTag();
-		}
-		
-		
-		
-		Log.i(TAG, "outer width is "+imageSimpleBean.getWidth()+" height is "+imageSimpleBean.getHeight());
-		
-		ImageLoader.getInstance().displayImage(imageSimpleBean.getUrl(), holder.ivIcon,ImageLoaderHelper.getInstance().getDefaultOptions(),
-				new ImageLoadingListener() {
-					@Override
-					public void onLoadingStarted(String imageUri, View view) {
-						holder.ivIcon.setImageDrawable(drawable);
-						holder.pbLoad.setVisibility(View.VISIBLE);
-					}
+    @Override
+    public View getView(final int position, View view, ViewGroup group) {
+        final ImageSimpleBean imageSimpleBean = list.get(position);
 
-					@Override
-					public void onLoadingFailed(String imageUri, View view,
-							FailReason failReason) {
-						String message = null;
-						switch (failReason.getType()) {
-						case IO_ERROR:
-							message = "Input/Output error";
-							break;
-						case DECODING_ERROR:
-							message = "can not be decoding";
-							break;
-						case NETWORK_DENIED:
-							message = "Downloads are denied";
-							break;
-						case OUT_OF_MEMORY:
-							message = "内存不足";
-							Toast.makeText(mContext, message, Toast.LENGTH_SHORT)
-									.show();
-							break;
-						case UNKNOWN:
-							message = "Unknown error";
-							Toast.makeText(mContext, message, Toast.LENGTH_SHORT)
-									.show();
-							break;
-						}
-						holder.pbLoad.setVisibility(View.GONE);
-					}
+        if (view == null) {
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            view = inflater.inflate(R.layout.image_item, null);
+        }
 
-					@Override
-					public void onLoadingComplete(String imageUri, View view,
-							Bitmap loadedImage) {
+        final ViewHolder holder = new ViewHolder(view);
+        Log.i(TAG, "outer width is " + imageSimpleBean.getWidth() + " height is " + imageSimpleBean.getHeight());
+
+        ImageLoader.getInstance().displayImage(imageSimpleBean.getUrl(), holder.rowIcon,
+                new ImageLoadingListener() {
+                    @Override
+                    public void onLoadingStarted(String imageUri, View view) {
+                        holder.pbLoad.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onLoadingFailed(String imageUri, View view,
+                                                FailReason failReason) {
+                        String message = null;
+                        switch (failReason.getType()) {
+                            case IO_ERROR:
+                                message = "Input/Output error";
+                                break;
+                            case DECODING_ERROR:
+                                message = "can not be decoding";
+                                break;
+                            case NETWORK_DENIED:
+                                message = "Downloads are denied";
+                                break;
+                            case OUT_OF_MEMORY:
+                                message = "内存不足";
+                                Toast.makeText(mContext, message, Toast.LENGTH_SHORT)
+                                        .show();
+                                break;
+                            case UNKNOWN:
+                                message = "Unknown error";
+                                Toast.makeText(mContext, message, Toast.LENGTH_SHORT)
+                                        .show();
+                                break;
+                        }
+                        holder.pbLoad.setVisibility(View.INVISIBLE);
+                    }
+
+                    @Override
+                    public void onLoadingComplete(String imageUri, View view,
+                                                  Bitmap loadedImage) {
 
                         int height = loadedImage.getHeight();
                         int width = loadedImage.getWidth();
-                        double scale =(double) (mScreenWidth/2)/width;
-                        int currentWidth = (int)(width * scale);
-                        int currentHeight =  (int)(height * scale);
+                        double scale = (double) (mScreenWidth / 2) / width;
+                        int currentWidth = (int) (width * scale);
+                        int currentHeight = (int) (height * scale);
                         LayoutParams lp = (LayoutParams) view.getLayoutParams();
                         lp.width = currentWidth;
                         lp.height = currentHeight;
                         view.setLayoutParams(lp);
+                        holder.pbLoad.setVisibility(View.INVISIBLE);
+
+                    }
+
+                    @Override
+                    public void onLoadingCancelled(String paramString,
+                                                   View paramView) {
+                        holder.pbLoad.setVisibility(View.INVISIBLE);
+                    }
+                });
 
 
-                        holder.pbLoad.setVisibility(View.GONE);
+        holder.rowIcon.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("info", (Serializable) list);
+                intent.putExtra("url", mUrl);
+                intent.setClass(mContext, PhotoDetailActivity.class);
+                mContext.startActivity(intent);
+                Log.i("TAG", "image click");
+            }
+        });
+        return view;
+    }
 
-					}
 
-					@Override
-					public void onLoadingCancelled(String paramString,
-							View paramView) {
-					}
-				});
+    /**
+     * This class contains all butterknife-injected Views & Layouts from layout file 'image_item.xml'
+     * for easy to all layout elements.
+     *
+     * @author ButterKnifeZelezny, plugin for Android Studio by Avast Developers (http://github.com/avast)
+     */
+    static class ViewHolder {
+        @InjectView(R.id.row_icon)
+        ImageView rowIcon;
+        @InjectView(R.id.pb_load)
+        ProgressBar pbLoad;
 
-		
-		
-		
-		holder.ivIcon.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Intent intent = new Intent();
-				intent.putExtra("info",(Serializable)list);
-				intent.putExtra("url", mUrl);
-				intent.setClass(mContext, PhotoDetailActivity.class);
-				mContext.startActivity(intent);
-				Log.i("TAG", "image click");
-			}
-		});
-		return view;
-	}
-
-	class Holder {
-		public ImageView ivIcon;
-		public ProgressBar pbLoad;
-	}
-
+        ViewHolder(View view) {
+            ButterKnife.inject(this, view);
+        }
+    }
 }
 
