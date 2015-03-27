bac3e3ef6e5c7a51139e39ec1263c630725c8732
ihgoo <ihgoo2008@gmail.com>
2015-03-27 11:56:36 +0800

    add method

--- Rosi/app/src/main/java/com/ihgoo/rosi/utils/Misc.java
+++ Rosi/app/src/main/java/com/ihgoo/rosi/utils/Misc.java
@@ -2,6 +2,8 @@ package com.ihgoo.rosi.utils;
 
 import android.content.Context;
 
+import com.ihgoo.rosi.BaseApplication;
+
 /**
  * Created by ihgoo on 2015/3/26.
  */
@@ -12,5 +14,18 @@ public class Misc {
     public static int px2dip(Context context,float pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dip2px(float dpValue) {
+        final float scale = BaseApplication.getInstance().getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static int dip2px(Double dpValue) {
+        final float scale = BaseApplication.getInstance().getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
     }
 }
